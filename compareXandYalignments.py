481608e19d71709b862ded72f891a69a0b4a938a
dans888 <43077619+dans888@users.noreply.github.com>
2020-07-27 16:21:04 -0400

    Create compareXandYalignments.py

---
+++ compareXandYalignments.py
@@ -0,0 +1,163 @@
+####input two psl files, output all matches of query file in 1.psl and 2.psl, print full line of 1.psl, and qname, qsize, qstart, qend, tname, tstart, tend of 2.psl in new file.
+
+def pull_data():
+        data = []
+        peaks = []
+        with open("Ymale.all.tissues.filtered.converted.psl", 'r') as macs_data:
+                for line in macs_data:
+                        if line.startswith("#"):
+                                continue
+                        else:
+                                data += line.split()
+                match=data[0::21]
+                mismatch=data[1::21]
+                repmatch=data[2::21]
+                Ns=data[3::21]
+                Qgap=data[4::21]
+                qgapbases=data[5::21]
+                Tgapcount=data[6::21]
+                Tgapbases=data[7::21]
+                strand=data[8::21]
+                Qname=data[9::21]
+                Qsize=data[10::21]
+                Qstart=data[11::21]
+                Qend=data[12::21]
+                Tname=data[13::21]
+                Tsize=data[14::21]
+                Tstart=data[15::21]
+                Tend=data[16::21]
+                blockcount=data[17::21]
+                blocksizes=data[18::21]
+                qStarts=data[19::21]
+                tStarts=data[20::21]
+                for index,value in enumerate(match):
+                        peaks.append(value)
+                        peaks.append(mismatch[index])
+                        peaks.append(repmatch[index])
+                        peaks.append(Ns[index])
+                        peaks.append(Qgap[index])
+                        peaks.append(qgapbases[index])
+                        peaks.append(Tgapcount[index])
+                        peaks.append(Tgapbases[index])
+                        peaks.append(strand[index])
+                        peaks.append(Qname[index])
+                        peaks.append(Qsize[index])
+                        peaks.append(Qstart[index])
+                        peaks.append(Qend[index])
+                        peaks.append(Tname[index])
+                        peaks.append(Tsize[index])
+                        peaks.append(Tstart[index])
+                        peaks.append(Tend[index])
+                        peaks.append(blockcount[index])
+                        peaks.append(blocksizes[index])
+                        peaks.append(qStarts[index])
+                        peaks.append(tStarts[index])
+        return peaks
+
+
+def pull_data2():
+        data = []
+        XY = []
+        with open("XIXmale.all.tissues.filtered.converted.psl", 'r') as macs_data:
+                for line in macs_data:
+                        if line.startswith("#"):
+                                continue
+                        else:
+                                data += line.split()
+                match=data[0::21]
+                mismatch=data[1::21]
+                repmatch=data[2::21]
+                Ns=data[3::21]
+                Qgap=data[4::21]
+                qgapbases=data[5::21]
+                Tgapcount=data[6::21]
+                Tgapbases=data[7::21]
+                strand=data[8::21]
+                Qname=data[9::21]
+                Qsize=data[10::21]
+                Qstart=data[11::21]
+                Qend=data[12::21]
+                Tname=data[13::21]
+                Tsize=data[14::21]
+                Tstart=data[15::21]
+                Tend=data[16::21]
+                blockcount=data[17::21]
+                blocksizes=data[18::21]
+                qStarts=data[19::21]
+                tStarts=data[20::21]
+                for index,value in enumerate(match):
+                        XY.append(value)
+                        XY.append(mismatch[index])
+                        XY.append(repmatch[index])
+                        XY.append(Ns[index])
+                        XY.append(Qgap[index])
+                        XY.append(qgapbases[index])
+                        XY.append(Tgapcount[index])
+                        XY.append(Tgapbases[index])
+                        XY.append(strand[index])
+                        XY.append(Qname[index])
+                        XY.append(Qsize[index])
+                        XY.append(Qstart[index])
+                        XY.append(Qend[index])
+                        XY.append(Tname[index])
+                        XY.append(Tsize[index])
+                        XY.append(Tstart[index])
+                        XY.append(Tend[index])
+                        XY.append(blockcount[index])
+                        XY.append(blocksizes[index])
+                        XY.append(qStarts[index])
+                        XY.append(tStarts[index])
+        return XY
+
+def compare1():
+        mvf=pull_data()
+        xandy=pull_data2()
+        match=mvf[0::21]
+        mismatch=mvf[1::21]
+        repmatch=mvf[2::21]
+        Ns=mvf[3::21]
+        Qgap=mvf[4::21]
+        qgapbases=mvf[5::21]
+        Tgapcount=mvf[6::21]
+        Tgapbases=mvf[7::21]
+        strand=mvf[8::21]
+        Qname=mvf[9::21]
+        Qsize=mvf[10::21]
+        Qstart=mvf[11::21]
+        Qend=mvf[12::21]
+        Tname=mvf[13::21]
+        Tsize=mvf[14::21]
+        Tstart=mvf[15::21]
+        Tend=mvf[16::21]
+        blockcount=mvf[17::21]
+        blocksizes=mvf[18::21]
+        qStarts=mvf[19::21]
+        tStarts=mvf[20::21]
+        bmatch=xandy[0::21]
+        bmismatch=xandy[1::21]
+        brepmatch=xandy[2::21]
+        bNs=xandy[3::21]
+        bQgap=xandy[4::21]
+        bqgapbases=xandy[5::21]
+        bTgapcount=xandy[6::21]
+        bTgapbases=xandy[7::21]
+        bstrand=xandy[8::21]
+        bQname=xandy[9::21]
+        bQsize=xandy[10::21]
+        bQstart=xandy[11::21]
+        bQend=xandy[12::21]
+        bTname=xandy[13::21]
+        bTsize=xandy[14::21]
+        bTstart=xandy[15::21]
+        bTend=xandy[16::21]
+        bblockcount=xandy[17::21]
+        bblocksizes=xandy[18::21]
+        bqStarts=xandy[19::21]
+        btStarts=xandy[20::21]
+        x=0
+        with open("YandXtranscripts", 'a') as out:
+                while x < len(bmatch):
+                        for index,value in enumerate(match):
+                                if str(Qname[index]) == str(bQname[x]):
+                                        out.write(str(match[index]) + '\t' + str(mismatch[index]) + '\t' + str(repmatch[index]) + '\t' + str(Ns[index]) + '\t' + str(Qgap[index]) + '\t' + str(qgapbases[index]) + '\t' + str(Tgapcount[index]) + '\t' + str(Tgapbases[index]) + '\t' + str(strand[index]) + '\t' + str(Qname[index]) + '\t' + str(Qsize[index]) + '\t' + str(Qstart[index]) + '\t' + str(Qend[index]) + '\t' + str(Tname[index]) + '\t' + str(Tsize[index]) + '\t' + str(Tstart[index]) + '\t' + str(Tend[index]) + '\t' + str(bmismatch[x]) + '\t' + str(bQname[x]) + '\t' + str(bQsize[x]) + '\t' +  str(bQstart[x]) + '\t' + str(bQend[x]) + '\t' + str(bTname[x]) + '\t' + str(bTstart[x]) + '\t' + str(bTend[x]) + '\n')
+                        x+=1
